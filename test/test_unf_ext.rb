1e339ed7dfda2d03849cf7643c4c9ea8c9c52b56
Akinori MUSHA <knu@idaemons.org>
2011-10-24 23:28:17 +0900

    Test all combinations.

--- test/test_unf_ext.rb
+++ test/test_unf_ext.rb
@@ -13,10 +13,29 @@ class TestUnf < Test::Unit::TestCase
     open(Pathname(__FILE__).dirname + 'normalization-test.txt', 'r:utf-8').each_slice(6) { |lines|
       flunk "broken test file" if lines.size != 6 || lines.pop !~ /^$/
       str, nfd, nfc, nfkd, nfkc = lines
-      assert nfd,  normalizer.normalize(str, :nfd)
-      assert nfc,  normalizer.normalize(str, :nfd)
-      assert nfkd, normalizer.normalize(str, :nfkd)
-      assert nfkc, normalizer.normalize(str, :nfkc)
+      assert nfd,  normalizer.normalize(str,  :nfd)
+      assert nfd,  normalizer.normalize(nfd,  :nfd)
+      assert nfd,  normalizer.normalize(nfc,  :nfd)
+      assert nfkd, normalizer.normalize(nfkc, :nfd)
+      assert nfkd, normalizer.normalize(nfkc, :nfd)
+
+      assert nfc,  normalizer.normalize(str,  :nfd)
+      assert nfc,  normalizer.normalize(nfd,  :nfc)
+      assert nfc,  normalizer.normalize(nfc,  :nfc)
+      assert nfkc, normalizer.normalize(nfkc, :nfc)
+      assert nfkc, normalizer.normalize(nfkd, :nfc)
+
+      assert nfkd, normalizer.normalize(str,  :nfkd)
+      assert nfkd, normalizer.normalize(nfd,  :nfkd)
+      assert nfkd, normalizer.normalize(nfc,  :nfkd)
+      assert nfkd, normalizer.normalize(nfkc, :nfkd)
+      assert nfkd, normalizer.normalize(nfkd, :nfkd)
+
+      assert nfkc, normalizer.normalize(str,  :nfkc)
+      assert nfkc, normalizer.normalize(nfd,  :nfkc)
+      assert nfkc, normalizer.normalize(nfc,  :nfkc)
+      assert nfkc, normalizer.normalize(nfkc, :nfkc)
+      assert nfkc, normalizer.normalize(nfkd, :nfkc)
     }
   end
 end
